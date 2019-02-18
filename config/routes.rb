b0ad590daa249f3b8143951ccb2faaed6ae99695
Nick Inman <nick@theoatmeal.com>
2019-02-18 05:21:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :authors, only: [:show] do 
     resources :authors, only [:show, :index]
-  end 
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
